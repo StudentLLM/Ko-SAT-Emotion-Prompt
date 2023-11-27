30c905b9aee00fc99aaa5e0c59c5798e82fb945a
YooYunS <141199486+YooYunS@users.noreply.github.com>
2023-11-27 16:51:54 +0900

    Update and rename main.py to korean_solve.py

--- korean_solve.py
+++ korean_solve.py
@@ -7,7 +7,7 @@ import openai
 from dotenv import load_dotenv
 from tqdm import tqdm
 
-from prompts import basic_prompt, literature_prompt, grammar_prompt
+from korean_prompts import basic_prompt, literature_prompt, grammar_prompt
 
 OPENAI_MODELS = [
     "gpt-4-1106-preview",
