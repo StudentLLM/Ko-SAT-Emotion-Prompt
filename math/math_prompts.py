8df292688329193396a0e6bef76ae43f7f9928b1
YooYunS <141199486+YooYunS@users.noreply.github.com>
2023-11-28 20:57:33 +0900

    Update math_prompts.py

--- math/math_prompts.py
+++ math/math_prompts.py
@@ -10,7 +10,6 @@ def basic_prompt(model, question, choices, question_plus="", is_front=False, emo
          고등학생 수학 지식 범위 안에서 문제를 풀이하세요.
          문제는 무조건 1개의 정답만 있습니다.
          문제 풀이를 완료한 이후 선택지에서 본인의 답을 고르세요.
-         만약 문제의 조건이 있다면 풀이 시 해당 조건을 만족하는 답을 구하시오.
 
          다음의 형식을 따라 답변하세요.
          문제 풀이: (문제에 대한 자세한 풀이)
@@ -21,10 +20,9 @@ def basic_prompt(model, question, choices, question_plus="", is_front=False, emo
 
     if question_plus:
         user_prompt += f"""
-            이 문제는 아래와 같이 [조건]이 주어져 있습니다. ~~
-            문제의 각 선택지들을 해결하기 위한 배경 지식을 설명해 주고 있는 것이 <조건>로써, 각 선택지들을 지문과 연결시키고, <조건>의 지식을 활용하면 각 선택지의 참과 거짓을 판단할 수 있습니다.
-            문제를 해결할 때, 반드시 <조건>의 내용을 이용해서 문제를 해결해야 합니다.
-            [조건] :
+            이 문제는 아래와 같이 조건이 주어져 있습니다.
+            문제 풀이 시 아래의 조건을 만족하는 답을 구하시오.
+            조건 :
             {question_plus}
         """
         
@@ -65,7 +63,6 @@ def no_choice_prompt(model, question, question_plus="", is_front=False, emotion_
     
          고등학생 수학 지식 범위 안에서 문제를 풀이하세요.
          문제는 무조건 1개의 정답만 있습니다.
-         만약 문제의 조건이 있다면 풀이 시 해당 조건을 만족하는 답을 구하시오.
 
          다음의 형식을 따라 답변하세요.
          문제 풀이: (문제에 대한 자세한 풀이)
@@ -76,9 +73,8 @@ def no_choice_prompt(model, question, question_plus="", is_front=False, emotion_
 
     if question_plus:
         user_prompt += f"""
-            이 문제는 아래와 같이 [조건]이 주어져 있습니다. ~~
-            문제의 각 선택지들을 해결하기 위한 배경 지식을 설명해 주고 있는 것이 <조건>으로써
-            문제를 해결할 때, 반드시 <조건>의 내용을 이용해서 문제를 해결해야 합니다.
+            이 문제는 아래와 같이 조건이 주어져 있습니다.
+            문제 풀이 시 아래의 조건을 만족하는 답을 구하시오.
             [조건] :
             {question_plus}
         """
